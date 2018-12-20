24436a942604fa4e0c176e0a19fcec2de575746a
Noah A <nanderson815@gmail.com>
2018-12-20 14:53:02 -0500

    Updated 12 20 18

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,5 +1,19 @@
 
 $(document).ready(function(){
-    console.log("you didnt fuck up!");
+
+var sports = ["baseball", "soccer", "football", "hockey", "golf", "swimming", "Lacrosse", "tennis", "basketball"];
+
+function makeButtons(){
+    $(".sportButtons").empty();
+    for (sport in sports){
+    var newButton = $("<button>");
+    newButton.attr("data-name", sports[sport]);
+    newButton.text(sports[sport]);
+    $(".sportButtons").append(newButton);
+    }
+}
+
+makeButtons();
+
 
 });
