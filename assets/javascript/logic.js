616a13d9723f871b4f95c795f8edb3ad20436759
Noah A <nanderson815@gmail.com>
2018-12-21 12:07:42 -0500

    Updated 12 21 18

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -5,13 +5,46 @@ $(document).ready(function () {
 
     function displaySportInfo() {
         var sport = $(this).attr("data-name");
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + sport + "&api_key=zaEEu57nzDtnWvOM40Q8fAzHUftNNQfP";
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + sport + "&api_key=zaEEu57nzDtnWvOM40Q8fAzHUftNNQfP&limit=10&raing=g";
 
         $.ajax({
             url: queryURL,
             method: "GET",
         }).then(function (response) {
             console.log(response);
+            $(".sportGifs").empty();
+
+            // for loop for all gifs grabbed in data.
+            for (i = 0; i < response.data.length; i++) {
+
+                // Create an  div
+                var gifDiv = $("<div>");
+
+                // Grab the rating and prepend to the div
+                var rating = response.data[i].rating;
+                var ratingDiv = $("<p>").text("Rating: "+rating);
+                gifDiv.prepend(ratingDiv);
+
+                // Create an image 
+                var image = $("<img>");
+
+                // Grab the still image and set it as data-still for IMG
+                image.attr("data-still", response.data[i].images.original_still).url;
+
+                // set the SRC of IMG to data-still
+                image.attr("src", response.data[i].images.original_still.url)
+
+                // Grab the gif and set it as data-animated
+                image.attr("data-animated", response.data[i].images.original.url);
+
+
+                // Append Img to the div.
+                gifDiv.append(image);
+
+                // prepend gifDiv to the page
+                $(".sportGifs").prepend(gifDiv);
+            }
+
         });
     }
 
@@ -33,10 +66,7 @@ $(document).ready(function () {
 
         if ($("#sportText").val().length !== 0) {
             sports.push($("#sportText").val().trim());
-
             makeButtons();
-
-
             $("#sportText").val('');
         }
     });
